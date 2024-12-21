091e550f1eb264bba5eeb879ac4447f9660983fc
Rahul Shah <rahul.shah9863@gmail.com>
2024-12-21 20:22:19 +0545

    lint: formatted code

--- src/chains.ts
+++ src/chains.ts
@@ -1,13 +1,12 @@
 export enum ChainId {
-  ODYSSEY = 1516,
+  ODYSSEY = 1516
 }
 
-export const SUPPORTED_CHAINS = [
-  ChainId.ODYSSEY,
-] as const
-export type SupportedChainsType = (typeof SUPPORTED_CHAINS)[number]
+export const SUPPORTED_CHAINS = [ChainId.ODYSSEY] as const
+
+export type SupportedChainsType = typeof SUPPORTED_CHAINS[number]
 
 export enum NativeCurrencyName {
   // Strings match input for CLI
-  IP = 'IP',
+  IP = 'IP'
 }
