718d62560b73be5334bdb0b57d9dfb3c2ea26ee6
thegaslessninja <thegaslessninja@gmail.com>
2025-02-10 13:58:21 +0545

    add: new chains

--- src/entities/wip9.ts
+++ src/entities/wip9.ts
@@ -1,8 +1,11 @@
+import { ChainId } from '../chains'
 import { Token } from './token'
 
 /**
  * Known WIP9 implementation addresses, used in our implementation of IP#wrapped
  */
 export const WIP9: { [chainId: number]: Token } = {
-  1516: new Token(1516, '0x1516000000000000000000000000000000000000', 18, 'WIP', 'Wrapped IP')
+  [ChainId.STORY]: new Token(ChainId.STORY, `${ChainId.STORY}000000000000000000000000000000000000`, 18, 'WIP', 'Wrapped IP'),
+  [ChainId.ODYSSEY]: new Token(ChainId.ODYSSEY, `${ChainId.ODYSSEY}000000000000000000000000000000000000`, 18, 'WIP', 'Wrapped IP'),
+  [ChainId.AENEID]: new Token(ChainId.AENEID, `${ChainId.AENEID}000000000000000000000000000000000000`, 18, 'WIP', 'Wrapped IP')
 }
